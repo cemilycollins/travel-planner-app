3e9149c5bf39374af681256d4563e939186f265a
finale100 <finale150@gmail.com>
2018-08-21 17:48:41 -0400

    added sidebar function

---
+++ frontend/src/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+  fetchTrips()
+}
+
+function fetchTrips() {
+  fetch('http://localhost:3000/trips')
+  .then(response => response.json())
+  .then(json => {
+    for(let trip of json) {
+      renderSideBar(trip)
+    }
+  })
+}
+
+function renderSideBar(trip) {
+  let sideBar = document.getElementById('invertedMenu')
+  let a = document.createElement('a')
+  a.addEventListener('click', renderTripProfile)
+  a.classList.add("active", "item")
+  sideBar.appendChild(a)
+  a.innerText = trip.name
+  a.dataset.id = trip.id
+}
+
+function renderTripProfile(event) {
+
+}
